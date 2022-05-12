2af2e049d12945f21bcacaa3fff645b2e14b2791
git-hyagi <45576767+git-hyagi@users.noreply.github.com>
2022-05-12 00:52:20 -0300

    testing

--- molecule/scenario_resources/tests/testinfra_test.py
+++ molecule/scenario_resources/tests/testinfra_test.py
@@ -4,17 +4,19 @@ def test_etc_directory(host):
     etc = host.file("/etc")
     assert etc.user == "root"
 
-# Didn't test with /opt/pulp/home because it was not getting created
-# E        +  where 1 = CommandResult(command=b'stat -c %U /opt/pulp/home', exit_status=1, stdout=None, stderr=b"stat: cannot stat '/opt/pulp/home': No such file or directory\n").rc
-# opt/pulp/lib/lib64/python3.8/site-packages/testinfra/modules/base.py:49: AssertionError
-@pytest.mark.parametrize("directory", [
-#    "/var/lib/pulp", "/opt/pulp/home"
-    "/var/lib/pulp"
-])
-def test_pulp_directories(host,directory):
-  pulp_dir = host.file(directory)
-  assert pulp_dir.user == "pulp"
-  assert pulp_dir.group == "pulp"
+def test_pulp_home(host):
+
+  pulp_home = host.file("/opt/pulp/home")
+  pulp_dir = host.file("/var/lib/pulp")
+
+  if pulp_home.exists:
+    assert pulp_home.user == "pulp"
+    assert pulp_home.group == "pulp"
+  elif pulp_dir.exists:
+    assert pulp_dir.user == "pulp"
+    assert pulp_dir.group == "pulp"
+  else:
+    assert False
 
 
 @pytest.mark.parametrize("service", [
@@ -37,7 +39,7 @@ def test_port_80(host,port):
 # WORKAROUND because testinfra does not have a builtin
 # module to handle HTTP requests
 def test_pulp_status_redirect(host):
-    output = host.check_output("curl http://localhost/pulp/api/v3/status/ -kw '%{http_code}'")
+    output = host.check_output("curl http://localhost/pulp/api/v3/status/ -o/dev/null -skw '%{http_code}'")
     assert '301' in output
 
 def test_pulp_status_max_1_redirect(host):
